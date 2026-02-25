98de370edc7b1397612a665e77d7bff08964cc9b
leydiuaq <lrobles15@alumnos.uaq.mx>
2026-02-25 10:03:49 -0600

    Initial commit

--- Repositorios.R
+++ Repositorios.R
@@ -1,2 +1,4 @@
 usethis::use_git()
 #No cargara toda la libreria, unicamente solo tilozara una funcion especifica de ese paquete. 
+
+#Acabo de crear un repositorio y lo guardare para que no este vacio. 
