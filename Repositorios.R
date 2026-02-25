1ee5412b1b86cb675f57bf6a636cae0ebbda6787
leydiuaq <lrobles15@alumnos.uaq.mx>
2026-02-25 10:08:51 -0600

    Genere un boxplot sencillo como prueba de que recuerdo como hacer commits y repositorios en github.

--- Repositorios.R
+++ Repositorios.R
@@ -2,3 +2,10 @@ usethis::use_git()
 #No cargara toda la libreria, unicamente solo tilozara una funcion especifica de ese paquete. 
 
 #Acabo de crear un repositorio y lo guardare para que no este vacio. 
+
+x<- rnorm(10000)
+
+pdf("03_results/boxplotx.pdf")
+boxplot(x, ncol = "purple", main = "Boxplot de X", xlab = "x", ylab = "values of X")
+dev.off()
+
