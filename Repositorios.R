b66d9c34e23bf6e31bde6f8dd6af97748943cadc
leydiuaq <lrobles15@gmail.com>
2026-02-25 10:25:13 -0600

    Initial commit

--- Repositorios.R
+++ Repositorios.R
@@ -6,6 +6,16 @@ usethis::use_git()
 x<- rnorm(10000)
 
 pdf("03_results/boxplotx.pdf")
-boxplot(x, ncol = "purple", main = "Boxplot de X", xlab = "x", ylab = "values of X")
+boxplot(x, ncol = "purple", main = "Boxplot de X", xlab = "x", ylab = "XX")
 dev.off()
+
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+usethis::use_git_config(
+  user.name = "leydiuaq",
+  user.email = "lrobles15@gmail.com"
+)
+##Informacion solo para que registre y vincule el repositorio a mis scripts. 
+usethis::use_github()
+
 
